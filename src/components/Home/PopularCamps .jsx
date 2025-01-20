34facd0dfd07b27f3f1f0ae88453a606789d60cc
tanvirahmedtagim <tanvirahmedtagim@gmail.com>
2025-01-20 18:58:12 +0600

    all work done

--- src/components/Home/PopularCamps .jsx
+++ src/components/Home/PopularCamps .jsx
@@ -71,7 +71,7 @@ const PopularCamps = () => {
               </p>
               <div className="flex justify-between items-center">
                 <button
-                  onClick={() => navigate(`/camp-details/${camp.id}`)}
+                  onClick={() => navigate(`/camp-details/${camp._id}`)}
                   className="bg-teal-500 text-white px-4 py-2 rounded hover:bg-teal-600"
                 >
                   Details
